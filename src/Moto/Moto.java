ce7674c77f00137c3b50aa1a699cf7c14042cb98
luannfirme <luannfirme@gmail.com>
2023-11-13 10:02:01 -0300

    Teste Commit

--- src/Moto/Moto.java
+++ src/Moto/Moto.java
@@ -2,4 +2,13 @@ package Moto;
 
 public class Moto {
 	private String fabricante;
+	private String Teste;
+
+	public String getFabricante() {
+		return fabricante;
+	}
+
+	public void setFabricante(String fabricante) {
+		this.fabricante = fabricante;
+	}
 }
